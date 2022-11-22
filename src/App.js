a0c111a4b2a56f93748688e79288d530789dd7b1
sandhya rani <snhchaudhary@gmail.com>
2022-11-23 00:39:20 +0530

    project

--- src/App.js
+++ src/App.js
@@ -1,48 +1,16 @@
 import logo from "./logo.svg";
 import "./App.css";
-import ClickCounter2 from "./components/ClickCounter";
-import HoverCounter2 from "./components/HoverCounter2";
-import User from "./components/User";
-import Counter2 from "./components/Counter2";
-// import ClickCounter from "./components/ClickCounter";
-// import HoverCounter from "./components/HoverCounter";
-// // import FRParentInput from "./components/FRParentInput";
-// // import PortalDemo from "./components/PortalDemo";
-// import Hero from "./components/Hero";
-// import ErrorBoundary from "./components/ErrorBoundary";
+import ComponentC from "./components/ComponentC";
+import { UserProvider } from "./components/userContext";
 
 
 function App() {
   return (
     <div className="App">
-    {/* <Counter2></Counter2>
-    //  {(count,incrementCount) => (
-    //   <ClickCounter2 count={count}> incrementCount={incrementCount} />
-    // )}
-    // <Counter2/>
-    // <Counter2>
-    // {(count,incrementCount) => (
-    //   <HoverCounter2 count={count}> incrementCount={incrementCount} />
-    // )}
-    </Counter2>
-    <ClickCounter2 />
-    <HoverCounter2 />
-    <User  render={(isLoggedIn)=>  isLoggedIn ?'Sandhya' : 'Guest'}/>
-    <ClickCounter  name='Sandhya'/>
-    <HoverCounte r />
-    <ErrorBoundary>
-    <Hero heroName="Batman"/>
-    </ErrorBoundary>
-
-    <ErrorBoundary>
-    <Hero heroName="Superman"/>
-    </ErrorBoundary>
-
-    <ErrorBoundary>
-    <Hero heroName="joker"/>
-    </ErrorBoundary>
-    <PortalDemo />
-<FRParentInput />*/}
+    <UserProvider>
+    <ComponentC />
+    </UserProvider>
+  
     </div>
   )
 }
